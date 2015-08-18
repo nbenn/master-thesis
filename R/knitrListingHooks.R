a251748c058f54689e47eb603c2810d41e7ddb3f
Nicolas Bennett <nicolas@projektneptun.ch>
2015-08-18 17:29:31 +0200

    add s3 objects subsection

--- R/knitrListingHooks.R
+++ R/knitrListingHooks.R
@@ -1,4 +1,12 @@
 knit_hooks$set(chunk = function(x, options) x)
 knit_hooks$set(source = function(x, options) {
-    paste0("\\begin{rcode}\n", x, "\n\\end{rcode}\n")
+  paste0("\\begin{rcode}\n", x, "\n\\end{rcode}\n")
+})
+knit_hooks$set(output = function(x, options) {
+  x <- unlist(strsplit(x, "\n"))
+  x <- gsub("^> ", "", x)
+  x <- gsub("^\\+ ", "", x)
+  x <- gsub("    ", "  ", x)
+  x <- paste(c(x, ""), collapse = "\n")
+  paste0("\\begin{rcode}\n", x, "\n\\end{rcode}\n")
 })
